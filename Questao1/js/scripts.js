21d10b2fd14e7a89a9729311b42eacf4396071ae
Samuel-dev-97 <samuelpssouza@gmail.com>
2025-06-22 21:45:23 -0300

    Atualização

--- Questao1/js/scripts.js
+++ Questao1/js/scripts.js
@@ -53,3 +53,76 @@ function quest1(){
     mediaIdadeCardio(vetor)
 }
 quest1()
+
+//resolução do professor da mesma questão//
+//***************************//
+//***************************//
+//***************************//
+function lerPacientes(pacientes) {
+    for (let i = 0; i < 5; i++) {
+        let objeto = {
+            nome: prompt(`Informe o nome do ${i + 1}º paciente:`),
+            idade: Number(prompt(`Informe a idade do ${i + 1}º paciente:`)),
+            especialidade: prompt(`Informe a especialidade do ${i + 1}º paciente ('cardiologia', 'pediatria' ou 'clínico-geral'):`).toLowerCase(),
+            pressao: parseFloat(prompt(`Informe a pressão do ${i + 1}º paciente (em mmHg):`))
+        };
+        pacientes.push(objeto);
+    }
+}
+
+function pacienteComMaiorPressao(pacientes) {
+    let maior = pacientes[0];
+    for (let i = 1; i < pacientes.length; i++) {
+        if (pacientes[i].pressao > maior.pressao) {
+            maior = pacientes[i];
+        }
+    }
+    return maior.nome;
+}
+
+function idososComPressaoAlta(pacientes) {
+    let contador = 0;
+    for (let i = 0; i < pacientes.length; i++) {
+        if (pacientes[i].idade > 60 && pacientes[i].pressao > 14.0) {
+            contador++;
+        }
+    }
+    return contador;
+}
+
+function mediaIdadeCardiologia(pacientes) {
+    let soma = 0;
+    let cont = 0;
+    for (let i = 0; i < pacientes.length; i++) {
+        if (pacientes[i].especialidade === "cardiologia") {
+            soma += pacientes[i].idade;
+            cont++;
+        }
+    }
+
+    if (cont === 0) {
+        return "Nenhum paciente de cardiologia.";
+    } else {
+        return (soma / cont).toFixed(2);
+    }
+}
+
+function sistemaClinica() {
+    let pacientes = [];
+    lerPacientes(pacientes);
+
+    const nomeMaiorPressao = pacienteComMaiorPressao(pacientes);
+    const qtdeIdososPressaoAlta = idososComPressaoAlta(pacientes);
+    const mediaIdadeCardio = mediaIdadeCardiologia(pacientes);
+
+    const resultado = `
+        Paciente com maior pressão arterial: ${nomeMaiorPressao}
+        \nQuantidade de pacientes com mais de 60 anos e pressão > 14.0: ${qtdeIdososPressaoAlta}
+        \nMédia de idade dos pacientes de cardiologia: ${mediaIdadeCardio}
+    `;
+
+    alert(resultado);
+}
+
+// Chame a função principal para executar o sistema
+sistemaClinica();
